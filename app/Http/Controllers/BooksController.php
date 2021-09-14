095eaae6b8d19a4d5a1ef68ceaca710913bf3031
Nurdin <nurdinuzakov@gmail.com>
2021-09-14 18:59:27 +0600

    first day commit

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Book;
+
+class BooksController extends Controller
+{
+    public function store()
+    {
+        Book::create([
+            'title' => request('title'),
+            'author' => request('author'),
+        ]);
+    }
+}
